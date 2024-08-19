f46a70b5f3a1dd274aafc042d370f0792aa0770b
darshchaurasia <darshchaurasia@gmail.com>
2024-08-19 16:49:44 -0700

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,9 @@ const Footer = () => {
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
         {/* Darsh Chaurasia Text on the Left */}
         <div className="text-left md:text-left order-1 md:order-1">
-          <p>Darsh Chaurasia</p>
+        <a href="https://www.darshchaurasia.com" target="_blank"  rel="noopener noreferrer" className="hover:underline">
+            Darsh Chaurasia
+          </a>
         </div>
 
         {/* Social Media Icons Centered Above Copyright */}
@@ -39,9 +41,9 @@ const Footer = () => {
 
         {/* Footer Links on the Right */}
         <div className="flex space-x-4 order-3 md:order-3">
-          <a href="https://www.darshchaurasia.com" className="hover:underline">
-            Contact Us
-          </a>
+        <a href="mailto:darshchaurasia@gmail.com?subject=Pathfinding%20Visualizer" className="hover:underline">
+  Contact Us
+</a>
         </div>
       </div>
     </footer>
