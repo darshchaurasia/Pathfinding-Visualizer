dfb770dd3f6335ac8c9ae8f126be9e3e1446fa26
darshchaurasia <darshchaurasia@gmail.com>
2024-08-19 22:54:31 -0700

    updated landing page height

--- src/components/aurora-background.tsx
+++ src/components/aurora-background.tsx
@@ -17,7 +17,7 @@ export const AuroraBackground = ({
     <main>
       <div
         className={cn(
-          "relative flex flex-col w-full h-[79.5vh] items-center justify-center bg-zinc-50 dark:bg-zinc-900  text-slate-950 transition-bg",
+          "relative flex flex-col w-full h-[76.9vh] items-center justify-center bg-zinc-50 dark:bg-zinc-900  text-slate-950 transition-bg",
           className
         )}
         {...props}
