7a1f6f5711c7190f1fc7dc6409a032962f1bd2a4
darshchaurasia <darshchaurasia@gmail.com>
2024-08-19 19:04:49 -0700

    underline navbar elements on hover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
-const Header = () => {
+const Header: React.FC = () => {
+  const [activeLink, setActiveLink] = useState<string>('');
+
+  const handleLinkClick = (link: string) => {
+    setActiveLink(link);
+  };
+
   return (
     <header className="w-full py-4 flex justify-between items-center px-8">
       <h1 className="text-2xl font-bold">Pathfinding Visualizer</h1>
       <nav>
         <ul className="flex space-x-6">
           <li>
-            <Link href="/" className="hover:text-white">Home</Link>
+            <Link href="/" className={`hover:underline ${activeLink === '/' ? 'underline' : ''}`} onClick={() => handleLinkClick('/')}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link href="/about" className="hover:text-white">About</Link>
+            <Link href="/about" className={`hover:underline ${activeLink === '/about' ? 'underline' : ''}`} onClick={() => handleLinkClick('/about')}>
+              About
+            </Link>
           </li>
           <li>
-            <Link href="/algorithms" className="hover:text-white">Algorithms</Link>
+            <Link href="/algorithms" className={`hover:underline ${activeLink === '/algorithms' ? 'underline' : ''}`} onClick={() => handleLinkClick('/algorithms')}>
+              Algorithms
+            </Link>
           </li>
           <li>
-            <Link href="/visualizer" className="hover:text-white">Visualizer</Link>
+            <Link href="/visualizer" className={`hover:underline ${activeLink === '/visualizer' ? 'underline' : ''}`} onClick={() => handleLinkClick('/visualizer')}>
+              Visualizer
+            </Link>
           </li>
-         {/* <li>
-            <a href="https://www.darshchaurasia.com"  target="_blank" className="hover:text-white">Contact</a>
-          </li>*/}
+          {/* Uncomment if you want to include the Contact link */}
+          {/* <li>
+            <Link href="https://www.darshchaurasia.com" target="_blank" className={`hover:underline ${activeLink === 'contact' ? 'underline' : ''}`} onClick={() => handleLinkClick('contact')}>
+              Contact
+            </Link>
+          </li> */}
         </ul>
       </nav>
     </header>
