584ad5f07ef2465e558d2462fc4648ab97e6d682
darshchaurasia <darshchaurasia@gmail.com>
2024-08-19 21:04:53 -0700

    fixed footer positioning

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,16 +5,16 @@ import { FaXTwitter } from 'react-icons/fa6';
 const Footer = () => {
   return (
     <footer className="w-full bg-black text-white py-6">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center md:items-center space-y-4 md:space-y-0 px-4">
         {/* Darsh Chaurasia Text on the Left */}
-        <div className="text-left md:text-left order-1 md:order-1">
-        <a href="https://www.darshchaurasia.com" target="_blank"  rel="noopener noreferrer" className="hover:underline">
+        <div className="flex-1 text-center md:text-left">
+          <a href="https://www.darshchaurasia.com" target="_blank" rel="noopener noreferrer" className="hover:underline">
             Darsh Chaurasia
           </a>
         </div>
 
-        {/* Social Media Icons Centered Above Copyright */}
-        <div className="flex flex-col items-center order-2 md:order-2">
+        {/* Social Media Icons and Copyright in the Center */}
+        <div className="flex-1 flex flex-col items-center text-center">
           <div className="flex space-x-4 mb-2">
             <a href="https://discord.gg/ury9rRMwk8" target="_blank" rel="noopener noreferrer">
               <FaDiscord size={24} />
@@ -32,18 +32,16 @@ const Footer = () => {
               <FaInstagram size={24} />
             </a>
           </div>
-
-          {/* Footer Copyright */}
-          <div className="text-center mt-2">
+          <div className="mt-2">
             <p>Copyright &copy; 2024 Pathfinding Visualizer. All rights reserved.</p>
           </div>
         </div>
 
         {/* Footer Links on the Right */}
-        <div className="flex space-x-4 order-3 md:order-3">
-        <a href="mailto:darshchaurasia@gmail.com?subject=Pathfinding%20Visualizer" className="hover:underline">
-  Contact Us
-</a>
+        <div className="flex-1 text-center md:text-right">
+          <a href="mailto:darshchaurasia@gmail.com?subject=Pathfinding%20Visualizer" className="hover:underline">
+            Contact Us
+          </a>
         </div>
       </div>
     </footer>
