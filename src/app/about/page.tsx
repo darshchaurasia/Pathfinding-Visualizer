28aa0d06cbad2777ef0883f8af6b93d1fc80c8d5
Darsh Chaurasia <darshchaurasia@gmail.com>
2024-10-17 21:12:21 -0700

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,8 +17,8 @@ const About = () => {
           <h1 className="text-4xl font-bold mb-4">About This Website</h1> {/* Increased to 4xl */}
           <p className="text-lg md:text-2xl leading-relaxed">
             This website is a pathfinding visualizer that demonstrates how various pathfinding algorithms like Dijkstra&apos;s and A* work. 
-            You can set start and end points on a grid, place obstacles, and watch as the algorithms find the shortest path in real-time. 
-            Explore different algorithms and learn how they efficiently navigate through the grid!
+            You can set place obstacles, and watch as the algorithms find the shortest path in real-time. 
+            Explore Djikstra and A* algorithms and learn how they efficiently navigate through the grid!
           </p>
         </div>
       </main>
